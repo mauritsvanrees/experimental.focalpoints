2bea5d25a6070266763e0e486a574b9b9f6f51a6
Maurits van Rees <maurits@vanrees.org>
2021-10-25 14:01:07 +0200

    Explain in readme why we require Py 3.7+: dataclasses.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.7",  # We use dataclasses, so 3.7+.
     install_requires=[
         "setuptools",
         "numpy",
